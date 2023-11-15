f03edbe5ccce152ca0e773dc9fd667ad697a57b5
theanh2906 <theanh2906@outlook.com>
2023-11-16 00:40:52 +0700

    initial commit

---
+++ src/main/java/com/example/demo/controllers/ChatController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Message;
+import com.example.demo.models.OutputMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/")
+public class ChatController {
+    @GetMapping("")
+    public ModelAndView home() {
+        return new ModelAndView("index");
+    }
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public OutputMessage send(final Message message) {
+        final String time = new SimpleDateFormat("HH:mm").format(new Date());
+        return new OutputMessage(message.getFrom(), message.getText(), time);
+    }
+}
